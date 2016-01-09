0cd13a1924038fc88d0d227131785cb23544ec98
Tomek Fijoł <tomekfijol@gmail.com>
2016-01-09 11:33:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,16 @@ var _ = require('lodash');
 var loaderUtils = require('loader-utils');
 
 module.exports = function (source) {
+
   this.cacheable && this.cacheable();
-  var opts = this.options._templateLoader = this.options._templateLoader || {};
+
+  var opts = this.options.underscoreTemplateLoader =
+    this.options.underscoreTemplateLoader || {};
   var queryOptions = loaderUtils.parseQuery(this.query);
   var template = _.template(source, queryOptions, opts.tplSettings);
   opts.engine = opts.engine || 'lodash';
-  return 'var _ = require(' + opts.engine + ');\n\nmodule.exports = ' + template + '\n';
+
+  return 'var _ = require(' + opts.engine + ');\n\nmodule.exports = ' +
+    template + '\n';
+
 };
