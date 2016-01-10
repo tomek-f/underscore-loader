562ff0bc3f9957794fbe53eae85cee0bba5a6573
Tomasz Fijoł <tomekfijol@gmail.com>
2016-01-10 03:48:09 +0100

    deps, move

---
+++ index.js
@@ -0,0 +1,37 @@
+var template = require('lodash.template');
+var merge = require('cmnjs/object/extend/deep');
+var loaderUtils = require('loader-utils');
+var minify = require('html-minifier').minify;
+
+var defaults = {
+  engine: 'lodash',
+  engineFull: '', 
+  minify: false,
+  minifierOptions: {
+    removeComments: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true
+  },
+  templateOptions: {}
+};
+
+module.exports = function (source) {
+
+  this.cacheable && this.cacheable();
+
+  var queryOptions = loaderUtils.parseQuery(this.query);
+  var config = merge({}, defaults, this.options.underscoreTemplateLoader,
+    { templateOptions: queryOptions });
+  var templateLocal;
+  
+  if (!config.engineFull) {
+    config.engineFull = 'var _ = require(\'' + config.engine + '\');\n\n';
+  }
+
+  templateLocal =
+    config.minify ? minify(source, config.minifierOptions) : source;
+  templateLocal = template(templateLocal, config.templateOptions);
+
+  return config.engineFull + 'module.exports = ' + templateLocal + ';\n';
+
+};
