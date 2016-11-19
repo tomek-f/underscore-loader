8d366976f6121f4d26cbdb1a3e748e72b874dc62
Tomasz Fijoł <tomekfijol@gmail.com>
2016-11-19 01:22:44 +0100

    args

--- extendDeep.js
+++ extendDeep.js
@@ -1,6 +1,6 @@
 const isObject = item => item && typeof item === 'object' && !Array.isArray(item);
 
-function mergeKeysObject(output, target, source, key) {
+function mergeKeysObject(target, source, output, key) {
   if (!(key in target)) {
     Object.assign(output, { [key]: source[key] });
   } else {
@@ -8,7 +8,7 @@ function mergeKeysObject(output, target, source, key) {
   }
 }
 
-function margeKeys(output, target, source, key) {
+function margeKeys(target, source, output, key) {
   if (isObject(source[key])) {
     mergeKeysObject(...arguments);
   } else {
@@ -19,7 +19,7 @@ function margeKeys(output, target, source, key) {
 function mergeDeepImmutable(target, source) {
   const output = Object.assign({}, target);
   if (!isObject(source) || !isObject(source)) return output;
-  Object.keys(source).forEach(k => margeKeys(output, target, source, k));
+  Object.keys(source).forEach(key => margeKeys(target, source, output, key));
   return output;
 }
 
