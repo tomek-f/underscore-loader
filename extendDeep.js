015531b8140c82eda3ef2ba1c6943a49b67cc429
Tomasz Fijoł <tomekfijol@gmail.com>
2016-11-19 01:24:18 +0100

    merge deep immutable example

--- extendDeep.js
+++ extendDeep.js
@@ -24,3 +24,8 @@ function mergeDeepImmutable(target, source) {
 }
 
 module.exports = mergeDeepImmutable;
+
+// const a = {a: 1, c: {d:2, c: 4, dd: {a: 1, b: 2, c: [2, 3, 4, 1, 1]}}};
+// const b = {b: 2, f: 1, c: {d:3, g: 4, dd: {be: 1, b: 3, c: [1, 2, 5, 4]}}};
+// const c = mergeDeepImmutable(a, b);
+// console.log(a, b, c);
